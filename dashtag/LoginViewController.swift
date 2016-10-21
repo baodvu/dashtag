3e3f863ecb39fec14806a57642520cb8cdc9e38c
Bao Vu <baodvu@gmail.com>
2016-10-21 16:09:19 -0400

    Add difficulty screen

--- dashtag/LoginViewController.swift
+++ dashtag/LoginViewController.swift
@@ -33,6 +33,20 @@ class LoginViewController: UIViewController {
         if let _ = FIRAuth.auth()?.currentUser {
             self.performSegueWithIdentifier("SegueToMain", sender: self)
         }
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(LoginViewController.dismissKeyboard))
+        
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tap)
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
